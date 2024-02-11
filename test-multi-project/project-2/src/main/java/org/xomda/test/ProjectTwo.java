b8c9fe5152f5b9212eaef59ed1ffaa1df01fe5f7
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-02-11 22:11:12 +0100

    [test-multi-project] fix models & java sources + .gitignore generates content

---
+++ test-multi-project/project-2/src/main/java/org/xomda/test/ProjectTwo.java
@@ -0,0 +1,12 @@
+package org.xomda.test;
+
+import project.two.Product;
+import project.two.ProductBean;
+
+public class ProjectTwo {
+
+	public static void main(String[] args) {
+		Product product = new ProductBean();
+	}
+
+}
