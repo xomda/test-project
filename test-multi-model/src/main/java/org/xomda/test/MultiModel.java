2c8430a6f694902bd8380d18e5a35e4bd28b8261
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-02-11 18:50:16 +0100

    fix

--- test-multi-model/src/main/java/org/xomda/test/MultiModel.java
+++ test-multi-model/src/main/java/org/xomda/test/MultiModel.java
@@ -1,6 +1,5 @@
 package org.xomda.test;
 
-import com.example.JteTest;
 import model.one.Customer;
 import model.one.CustomerBean;
 import model.two.Order;
@@ -9,8 +8,6 @@ import model.two.OrderBean;
 public class MultiModel {
 
 	public static void main(String[] args) {
-		JteTest test;
-
 		Customer customer = new CustomerBean();
 		Order order = new OrderBean();
 	}
