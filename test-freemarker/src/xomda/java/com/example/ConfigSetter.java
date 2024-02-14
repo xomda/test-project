2bc65a9f2eb269597c74c6a9eb800b80f7cffddd
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-02-14 10:06:35 +0100

    test-freemarker: write to JSON and test it

--- test-freemarker/src/xomda/java/com/example/ConfigSetter.java
+++ test-freemarker/src/xomda/java/com/example/ConfigSetter.java
@@ -9,6 +9,8 @@ import freemarker.template.TemplateModelException;
 
 public class ConfigSetter implements TemplateMethodModelEx {
 
+	public static String METHOD_NAME = "setConfig";
+
 	Map<String, Object> config;
 
 	public ConfigSetter(Map<String, Object> config) {
@@ -18,6 +20,9 @@ public class ConfigSetter implements TemplateMethodModelEx {
 	public TemplateModel exec(List args) throws TemplateModelException {
 		if (args.size() != 2) {
 			throw new TemplateModelException("Wrong arguments");
+		}
+		if (args.get(0) == METHOD_NAME) {
+			throw new TemplateModelException("Overwriting 'setConfig' is forbidden.");
 		}
 		config.put(args.get(0).toString(), args.get(1));
 		return TemplateModel.NOTHING;
